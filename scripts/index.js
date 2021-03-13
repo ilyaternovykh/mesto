19f859ce80a2fe04ccc446b391cde35fe21390d6
Ilya Ternovykh <ily1925@yandex.ru>
2021-03-13 16:57:56 +0300

    feat: реализовать анимацию попап

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,14 @@
 const showPopupProfile = document.querySelector('.profile__edit-button');
 const showPopupCards = document.querySelector('.profile__add-button');
-//let popup = document.querySelector('.popup');
 let popupProfile = document.querySelector('.popup_type_profile');
 let popupCards = document.querySelector('.popup_type_cards');
 const closePopupButtonProfile = popupProfile.querySelector('.popup__close_type_profile');
 const closePopupButtonCards = popupCards.querySelector('.popup__close_type_cards');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__container');// Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector('.popup__input_type_name');// Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector('.popup__input_type_job');// Воспользуйтесь инструментом .querySelector()
+let formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_job');
 let titleInput = popupCards.querySelector('.popup__input_type_title');
 let linkInput = popupCards.querySelector('.popup__input_type_image-link');
 
@@ -168,63 +165,8 @@ closePopupButtonCards.addEventListener('click', function() {
 closePopupButtonImage.addEventListener('click', function() {
   closePopup(popupImageContainer);
 })
-// function openPopup(popup) {
-//   // nameInput.value = profileTitle.textContent;
-//   // jobInput.value = profileSubtitle.textContent;
 
-//   popup.classList.add('popup_opened');
-// }
-
-// function closePopup() {
-//   popup.classList.remove('popup_opened');
-// }
-
-// function togglePopup(popup) {
-//   popup.classList.toggle('popup_opened');
-// }
-// function togglePopup(evt) {
-//   console.log(evt.target.closest('.popup'));
-//   //popup.classList.toggle('popup_opened');
-// }
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-
-
-// function formSubmitCards (evt) {
-//   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-//   console.log('Привет!')
-
-//   //closePopup(popupProfile);
-// }
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 popupProfile.addEventListener('submit', formSubmitHandler);
 popupCards.addEventListener('submit', addCardFormListener);
-
-// showPopupProfile.addEventListener('click', () => {
-//   openPopup(popupProfile);
-// });
-// // closePopupButtons.addEventListener('click', closePopup);
-// showPopupCards.addEventListener('click', () => {
-//   openPopup(popupCards);
-// });
-
-// showPopupProfile.addEventListener('click', togglePopup);
-
-// showPopupCards.addEventListener('click', () => {
-//   togglePopup;
-// });
-
-// closePopupButton.forEach((item) => {
-//   item.addEventListener('click', closePopup);
-//   console.log(item);
-//   console.log(closePopup);
-// });
-//showPopupCards.addEventListener('click', openPopup);
-
-// closePopupButton.addEventListener('click', () => { togglePopup(popupProfile); });
-// closePopupButton.addEventListener('click', () => { togglePopup(popupCards); });
 
 renderGrid();
